362357cbf83db2304c511c7c0a665e6e2a781dc9
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2025-09-08 20:35:52 +0400

    darktheme and main window

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import shortcut_manager
 import gui_manager
 import setup_manager
 import logging
+import qdarktheme
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
@@ -13,27 +14,29 @@ def main():
     logger.info("Initializing...")
 
     state.window, app = gui_manager.init_main_window()
+    # Dark theme
+    qdarktheme.setup_theme()
 
     if not setup_manager.validate_config():
         logger.info("Config invalid or missing, opening setup window...")
         setup_window = gui_manager.init_setup_window()
 
     # Get path to Steam installation
-    steam_path = state.steam_path
-    if steam_path == "":
-        logger.error("Steam not found")
-        # TODO: prompt to select steam path
-        sys.exit("no_steam")
-    else:
-        logger.info("Steam found at: %s", steam_path)
+    # steam_path = state.steam_path
+    # if steam_path == "":
+    #     logger.error("Steam not found")
+    #     # TODO: prompt to select steam path
+    #     sys.exit("no_steam")
+    # else:
+    #     logger.info("Steam found at: %s", steam_path)
 
     # Get logged in users
     # TODO: pick in setup
-    user = state.user
-    logger.info("Picked user: %s", user)
-    shortcuts_path = path_manager.get_shortcuts_path(steam_path, user)
+    # user = state.user
+    # logger.info("Picked user: %s", user)
 
     # Get shortcuts
+    shortcuts_path = path_manager.get_shortcuts_path(state.steam_path, state.user)
     shortcuts = shortcut_manager.get_existing_shortcuts(shortcuts_path)
     logger.info("Found %i shortcut entries", len(shortcuts))
 
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    # if not shortcuts:
-    #     logger.error("Error reading %s", shortcuts_path)
-    #     # TODO: show error dialog
-    #     #gui_manager.popup(window, "Error", "Error reading shortcuts.vdf")
-    #     sys.exit("shortcut_read_error")
